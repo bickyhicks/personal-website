5f7c5e525b5c6234b64e3ff72f493118bd6f015e
bickyhicks <68948631+bickyhicks@users.noreply.github.com>
2021-07-06 21:38:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
 
 
-window.onscroll = function () {navbarStick()};
-
-var navbar = document.getElementById("navbar");
-
-var sticky = navbar.offsetTop;
-
-function navbarStick() {
-  if (window.scrollY >= sticky){
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky")
-  }
-}
-
 $(".dropdown").click(function() {
   $(".navbar").addClass("responsive");
 
   $(".navbar a").click(function() {
     $(".navbar").removeClass("responsive");
   })
+});
+
+$('.dropdown-container').click(function(){
+  $(this).find('p').toggleClass("hide-info");
 });
